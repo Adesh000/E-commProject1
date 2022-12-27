8493e3542e284433659786e20c3cc1f07ef9ec80
Adesh Nayak <adeshnayak6@gmail.com>
2022-12-27 14:40:45 +0530

    dotenv added

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,57 +1,69 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authroles";
-import bcrypt from "bcryptjs"
-import JWT from "jsonwebtoken"
-import crypto from "crypto"
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import crypto from "crypto";
+import { config } from "dotenv";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Name is required"],
-        maxLength: [50, "Name must be less than 50 characters"]
+      type: String,
+      required: [true, "Name is required"],
+      maxLength: [50, "Name must be less than 50 characters"],
     },
     email: {
-        type: String,
-        required: [true, "Email is required"],
-        unique: true
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
     },
     password: {
-        type: String,
-        rquired: [true, "Password is required"],
-        minLength: [7, "Password can not be less than 7 characters"],
-        select: false
+      type: String,
+      rquired: [true, "Password is required"],
+      minLength: [7, "Password can not be less than 7 characters"],
+      select: false,
     },
     role: {
-        type: String,
-        enum: Object.values(AuthRoles),
-        default: AuthRoles.USER
+      type: String,
+      enum: Object.values(AuthRoles),
+      default: AuthRoles.USER,
     },
     forgotPasswordToken: String,
-    forgotPasswordExpiry: Date
-},
-{
-    timestamps: true
-})
+    forgotPasswordExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 //challenge 1 - encrypt password before saving it
 
-userSchema.pre("save", async function(next){
-    if(!this.modified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 //add more features directly to your schema
 userSchema.methods = {
-    //compare password
-    comparePassword: async function(enteredPassword){
+  //compare password
+  comparePassword: async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  },
 
-    },
+  //get jwt token
+  getJwtToken: function () {
+    return JWT.sign(
+      {
+        _id: this._id,
+        role: this.role,
+      },
+      config.JWT_SECRET,
+      {
+        expiresIn: config.JWT_EXPIRY,
+      }
+    );
+  },
+};
 
-    //get jwt token
-    getJwtToken: function(){
-        
-    }
-}
-
-export default mongoose.model("user", userSchema)
+export default mongoose.model("user", userSchema);
